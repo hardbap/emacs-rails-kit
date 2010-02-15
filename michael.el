0fecf02490cabfdba68afbbe9e0e26375b65fe30
Mike Breen <hardbap@gmail.com>
2010-02-15 17:12:49 -0500

    More customization

--- michael.el
+++ michael.el
@@ -1,6 +1,20 @@
-(setq mac-command-modifier 'meta)
+(setq mac-command-modifier 'meta) ; set CMD key to Meta key
+
 (require 'frame-cmds)
 (global-set-key (kbd "C-M-<up>") 'shrink-frame)
+(global-set-key (kbd "C-M-<down>") 'enlarge-frame)
+(global-set-key (kbd "C-M-<right>") 'enlarge-frame-horizontally)
+(global-set-key (kbd "C-M-<left>") 'shrink-frame-horizontally)
 
-(transient-mark-mode 1) ; makes the region visible
-(line-number-mode 1)    ; makes the line number show up
+
+(transient-mark-mode t) ; makes the region visible
+(setq save-abbrevs nil)
+
+(set-face-font 'default "-apple-Droid_Sans_Mono-medium-normal-normal-*-14-*-*-*-m-0-iso646-1")
+
+; sync kill ring and clipboard
+(global-set-key (kbd "C-w") 'clipboard-kill-region)
+(global-set-key (kbd "M-w") 'clipboard-kill-ring-save)
+(global-set-key (kbd "C-y") 'clipboard-yank)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
